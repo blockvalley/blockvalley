fc608d01926eb3dec7dff85b4bc0a0cf9b5db6ff
David LJ <david.lozano@btcassessors.com>
2019-01-04 13:32:33 +0100

    Modified account initialization script to display address

--- scripts/init_account.sh
+++ scripts/init_account.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 #
-# Initializes a geth node's docker volume with the given account private key
-# and password
-#
-# Given a docker volume that will be used as the geth data dir for a
-# geth node, initializes the data dir importing the given account using its
-# private key and a password to protect it.
+# Initializes a docker volume to be used as the data directory of a Geth node
+# with the given account private key encrypted by the given password
 #
 
 # Gather params
-source "$(dirname "$0")/params.rc"
+# shellcheck source=params.rc
+. "$(dirname "$0")/params.rc"
 
 # Arguments
 volume="$1"
@@ -18,30 +15,30 @@ password_file="$3"
 
 # Checks
 # # Mandatory arguments
-if [ "$volume" == "" ] || [ "$privkey_file" == "" ] || [ "$password_file" == "" ]; then
-	echo "Usage:"
-	echo "$0 <volume> <privkey_file> <password_file>"
-	echo
-	echo TL\;DR: runs \"geth account import --password \<password_file\>\
+if [ -z "$volume" ] || [ -z "$privkey_file" ] || [ -z "$password_file" ]; then
+	>&2 echo "Usage:"
+	>&2 echo "$0 <volume> <privkey_file> <password_file>"
+	>&2 echo
+	>&2 echo TL\;DR: runs \"geth account import --password \<password_file\>\
  \<privkey_file\>\" and writes the result in the specified docker volume | \
  fold -s
-	echo
-	echo Initializes the docker volume specified, filling the keystore \
+	>&2 echo
+	>&2 echo Initializes the docker volume specified, filling the keystore \
 directory with the import account | fold -s
-	echo
-	echo If the volume specified does not exist yet, will create a new one \
+	>&2 echo
+	>&2 echo If the volume specified does not exist yet, will create a new one \
 and import the account there | fold -s
 	exit 1
 fi
 
 # # Private key file exists
 if ! [ -f "$privkey_file" ]; then
-	echo "Private key file \"$privkey_file\" does not exist."
+	>&2 echo "Private key file \"$privkey_file\" does not exist."
 	exit 1
 fi
 # # Password file exists
 if ! [ -f "$password_file" ]; then
-	echo "Password file \"$password_file\" does not exist."
+	>&2 echo "Password file \"$password_file\" does not exist."
 	exit 1
 fi
 
@@ -50,46 +47,45 @@ privkey_filename=$(basename "$privkey_file")
 password_filename=$(basename "$password_file")
 
 # # Same filename
-if [ "$privkey_filename" == "$password_filename" ]; then
-	echo "Password and private key files must have different file names"
+if [ "$privkey_filename" = "$password_filename" ]; then
+	>&2 echo "Password and private key files must have different file names"
 	exit 1
 fi
 
-# Run container to `geth account import` in it :P
-container_id=$(docker run -dit \
-	--mount type=volume,source=$volume,destination=$ETH_DATA_DIR \
-	--entrypoint="/bin/sh" "$DOCKER_IMAGE_GETH"
-)
-[ $? -ne 0 ] && echo "Unable to run docker container" && exit 1
+# Run container to `geth account import` in it
+if ! container_id=$(docker run -dit \
+	--mount "type=volume,source=$volume,destination=$ETH_DATA_DIR" \
+	--entrypoint="/bin/sh" "$DOCKER_IMAGE_GETH"); then
+    >&2 echo "Unable to run docker container" && exit 1
+fi
 
 # # Container functions
-function clean {
-	docker rm -f "$container_id" &> /dev/null
+clean() {
+    docker rm -f "$container_id" > /dev/null 2>&1
+    if [ -n "$tmp_file" ]; then
+        rm "$tmp_file" || (>&2 echo Unable to delete temporal file && exit 1)
+    fi
+}
+run() { docker exec "$container_id" "$@"; }
+copy() {
+    if ! docker cp "$1" "$container_id:/root"; then
+        clean && echo >&2 "Unable to copy \"$1\" to container" && exit 1
+    fi
 }
 
-function run {
-	docker exec "$container_id" "$@"
-}
-
-function copy {
-	docker cp "$1" $container_id:/root
-	[ $? -ne 0 ] && clean && echo "Unable to copy \"$1\" to container" &&
-exit 1
-}
-
-# Copy account private key into container
+# Copy account private key and password into container
 copy "$privkey_file"
 copy "$password_file"
 
 # Execute geth import
-docker exec "$container_id" \
+# # Log result
+if ! tmp_file="$(mktemp)"; then >&2 echo Unable to create tmp file; fi
+# # Run
+if docker exec "$container_id" \
 	geth account import --password "/root/$password_filename" \
-	"/root/$privkey_filename"
-
-# Check execution
-if [ $? -eq 0 ]; then
-	echo Initialized volume with \"$privkey_file\" and \"$password_file\" in \
-keystore directory | fold -s
+	    "/root/$privkey_filename" > "$tmp_file" 2> /dev/null; then
+    # Display address
+    cut -d'{' -f2 "$tmp_file" | tr -d '}'
 	clean
 	exit 0
 else
